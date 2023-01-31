532e335e6fe2882a30e035a7e0b9b9d4c8d3ef33
mark.otting <mark.otting@vopak.com>
2023-01-31 23:14:41 +0100

    FUNCTION: Added get_highest function

--- highscore_server_barebones_on_sqlalch.py
+++ highscore_server_barebones_on_sqlalch.py
@@ -1,9 +1,11 @@
 from flask import Flask, send_from_directory, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import func
+from sqlalchemy.orm import Session
 
 app = Flask(__name__, static_url_path="/", static_folder="www")
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///scores.db'
 db = SQLAlchemy(app)
 CORS(app)
@@ -16,11 +18,17 @@ def result_to_dict(sql_result):
     return result_dict
 
 
-class Highscores(db.Model):
+class Highscore(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), nullable=False)
     score = db.Column(db.String(80), nullable=False)
     game = db.Column(db.String(120), nullable=False)
+
+    # This function is not used ..yet
+    @classmethod
+    def get_highest(self):
+        with Session(db.engine) as session:
+            print(session.query(self.name, func.max(Highscore.score)).first())
 
 
 @app.route("/highscores/<game>", methods=["GET", "POST"])
@@ -28,7 +36,7 @@ def handle_highscores(game):
     if request.method == "POST":
         body = request.json
         try:
-            score = Highscores(name=body["name"], score=body["score"], game=game)
+            score = Highscore(name=body["name"], score=body["score"], game=game)
             db.session.add(score)
             db.session.commit()
             result = "ok"
@@ -38,10 +46,9 @@ def handle_highscores(game):
             error = str(e)
         return jsonify({"result": result, "error": error})
     elif request.method == "GET":
-        scores = Highscores.query.filter_by(game=game)
+        scores = Highscore.query.filter_by(game=game)
         scores_dict = {"scores": result_to_dict(scores)}
         return jsonify(scores_dict)
-
 
 @app.route("/")
 @app.route("/jquery")
